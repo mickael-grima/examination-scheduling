aee28a84f7dac0e1bd0337ecf16345e867fad025
mickael@cliqz.com <mickael@cliqz.com>
2016-04-23 21:32:25 +0200

    colouring.py refactored and tests added

--- colouring.py
+++ colouring.py
@@ -1,83 +1,126 @@
 import networkx as nx
 import random as rd
 import numpy as np
-import os 
+import os
 
 import matplotlib
-matplotlib.use('Agg') # for not popping up windows
 import matplotlib.pyplot as plt
-
-# names for saving
-directory = "plots/"
-name = "graphcolouring"
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-
-# number of nodes
-n = 16
-G = nx.Graph()
-
-# construct random node-node-incidence matrix
-rands = np.matrix([[rd.randint(0,2) < 1 for i in range(n) ] for j in range(n)])
-
-# make the matrix symmetric
-for i in range(n):
-    rands[i,i] = True 
-    for j in range(i+1,n):
-        rands[j,i] = rands[i,j]
-
-# make edges 
-for i in range(n):
-    for j in range(i+1,n):
-        if(rands[i,j]):
-            G.add_edge(i,j)
-
+matplotlib.use('Agg')  # for not popping up windows
 all_colours = ["blue", "red", "yellow", "purple", "orange", "green", "grey", "cyan"]
 m = len(all_colours)
 
-# print start configuration
-colours = ["white"] * n
-nx.draw_shell(G, node_color = colours)
-plt.savefig(directory + name + "000.jpg")
-    
-# graph colouring greedy algorithm with degree heuristic
-degree = [ rands[i,range(n)].sum() - 1 for i in range(n) ]
 
-# sort by degree
-sl = sorted( zip(degree, range(n)))
-lookup_order = [ x[1] for x in sl ]
+class ColorGraph(object):
+    def __init__(self):
+        self.DIRECTORY = "plots/"
+        self.NAME = "graphcolouring"
+        self.graph = nx.Graph()
+        self.colours = {}
+        self.incidence_matrix = None
 
-# revert??
-lookup_order = reversed(lookup_order)
+        if not os.path.exists(self.DIRECTORY):
+            os.makedirs(self.DIRECTORY)
 
-def check_neighbours(node, matrix, colour, colours):
-    n = np.shape(matrix)[0]
-    assert(len(colours) >= n)
-    assert(node < n)
-    assert(n >= 0)
-    
-    for j in range(n):
-        if matrix[i,j] & (colours[j] == colour):
-            return(False)
-    return(True)
-        
-counter = 1
-for i in lookup_order:
-    for col in all_colours:
-        if check_neighbours(i, rands, col, colours):
-            colours[i] = col
-            nx.draw_shell(G, node_color = colours)
-            filename = directory + name
-            if(counter < 10) :
+    def add_node(self, node):
+        self.graph.add_node(node)
+        self.colours.setdefault(node, 'white')
+
+    def add_edge(self, node1, node2):
+        self.graph.add_edge(node1, node2)
+        self.colours.setdefault(node1, 'white')
+        self.colours.setdefault(node2, 'white')
+
+    def update_color(self, node, color):
+        if self.colours.get(node) is None:
+            return False
+        self.colours[node] = color
+        return True
+
+    def get_incidence_matrix(self):
+        """ Return the node-node-incidence matrix as a dct
+        """
+        incidence_matrix = {}
+        for edge in self.graph.edges():
+            incidence_matrix.setdefault(edge[0], set())
+            incidence_matrix.setdefault(edge[1], set())
+            incidence_matrix[edge[0]].add(edge[1])
+            incidence_matrix[edge[1]].add(edge[0])
+        return incidence_matrix
+
+    def get_degree(self):
+        """ return a dictionnary {node: degree}
+        """
+        incidence_matrix = self.get_incidence_matrix()
+        degree = {node: sum(list(incidence_matrix[node])) for node in self.graph.nodes()}
+        return degree
+
+    def check_neighbours(self, node, colour):
+        n = len(self.graph.nodes())
+        assert(len(self.colours) >= n)
+        assert(node in self.graph.nodes())
+        assert(n >= 0)
+
+        for j in self.graph.nodes():
+            if ((node, j) in self.graph.edges() or (j, node) in self.graph.edges()) & (self.colours[j] == colour):
+                return(False)
+        return(True)
+
+    def draw(self, save=False, ind=0):
+        colours = [colour for _, colour in self.colours.iteritems()]
+        nx.draw_shell(self.graph, node_color=colours)
+        if save:
+            filename = self.DIRECTORY + self.NAME
+            if(ind < 10):
                 filename = filename + '00'
-            elif(counter < 100) :
+            elif(ind < 100):
                 filename = filename + '0'
-            plt.savefig(filename + '%d.jpg' % (counter))
-            counter += 1
-            break
+            plt.savefig("%s%d.jpg" % (filename, ind))
 
-print(colours)
+    def build_rand_graph(self, nb_nodes=16):
+        # construct random node-node-incidence matrix
+        rands = np.matrix([[rd.randint(0, 2) < 1 for i in range(nb_nodes)]
+                          for j in range(nb_nodes)])
+        # make the matrix symmetric
+        for i in range(nb_nodes):
+            rands[i, i] = True
+            for j in range(i + 1, nb_nodes):
+                rands[j, i] = rands[i, j]
+        # make edges
+        for i in range(nb_nodes):
+            for j in range(i + 1, nb_nodes):
+                if(rands[i, j]):
+                    self.add_edge(i, j)
+        self.incidence_matrix = rands
+
+    def color_graph(self, save=False):
+        degree = self.get_degree()
+        # degree = [self.incidence_matrix[i, range(n)].sum() - 1 for i in range(n)]
+        # sort by degree
+        # sl = sorted(zip(degree, range(n)))
+        sl = sorted([(dg, node) for node, dg in degree.iteritems()])
+        lookup_order = [x[1] for x in sl]
+        # revert??
+        lookup_order = reversed(lookup_order)
+
+        counter = 1
+        for node in lookup_order:
+            for col in all_colours:
+                if self.check_neighbours(node, col):
+                    self.colours[node] = col
+                    self.draw(save=save, ind=counter)
+                    counter += 1
+                    break
+
+        return self.colours
+
+
+n = 16
+G = ColorGraph()
+G.build_rand_graph(nb_nodes=n)
+G.draw(save=True, ind=0)
+G.color_graph(save=True)
+
+print(G.colours)
 
 # convert to animation
 os.system("convert -delay 70 -loop 0 plots/*jpg animated.gif")
