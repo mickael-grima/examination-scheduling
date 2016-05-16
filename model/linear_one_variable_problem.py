d05e6c32fd06f061212868f43860f7f14ae3c118
wedgeCountry <halbig.roland@freenet.de>
2016-05-16 13:49:30 +0200

    corrected linear_one_variable_model

--- model/linear_one_variable_problem.py
+++ model/linear_one_variable_problem.py
@@ -16,10 +16,12 @@ from model.main_problem import MainProblem
 
 
 class LinearOneVariableProblem(MainProblem):
+    
     def __init__(self, name='ExaminationProblem'):
         super(LinearOneVariableProblem, self).__init__(name=name)
         self.c = 0.5  # criteria factor
         self.available_constants = ['s', 'c', 'Q', 'T', 'h']  # every constants names have to be included in this list
+
 
     def build_variables(self):
         """
@@ -33,31 +35,46 @@ class LinearOneVariableProblem(MainProblem):
                     self.vars['x'][i, k, l] = self.problem.addVar(vtype=gb.GRB.BINARY, name='x[%s,%s,%s]' % (i, k, l))
         return True
 
+
     def build_constraints(self):
         """
             Build the variables of the problem from the data
         """
         n, r, p = self.dimensions['n'], self.dimensions['r'], self.dimensions['p']
+            
+        # Exactly one period and one room per exam
         for i in range(n):
-            # Exactly one period and one room per exam
-            constraint = (gb.quicksum([self.vars['x'][i, k, l] for k in range(r) for l in range(p)]) == 1)
+            constraint = (gb.quicksum([self.vars['x'][i, k, l] for k in range(r) for l in range(p)]) >= 1)
             self.problem.addConstr(constraint, "c0")
-            # Enough seat for one exam
+            
+        # Enough seat for one exam
+        for i in range(n):
             constraint = (
                 gb.quicksum([self.vars['x'][i, k, l] * self.constants['c'][k] for k in range(r) for l in range(p)]) >=
                 self.constants['s'][i]
             )
             self.problem.addConstr(constraint, "c1")
-            for j in range(i + 1, n):
-                for l in range(p):
-                    # No conflicts
-                    constraint = (
-                        gb.quicksum([self.vars['x'][i, k, l] + self.vars['x'][j, k, l] for k in range(r)]) <=
-                        2 - self.constants['Q'][i][j]
-                    )
-                    self.problem.addConstr(constraint, "c2")
+       
+        # maximal one exam per room per period
         for k in range(r):
             for l in range(p):
-                # maximal one exam per room per period
-                constraint = (gb.quicksum([self.vars['x'][i, k, l] for i in range(n) for l in range(p)]) <= 1)
-                self.problem.addConstr(constraint, "c3")
+                constraint = (gb.quicksum([self.vars['x'][i, k, l] for i in range(n)]) <= self.constants['T'][k][l])
+                self.problem.addConstr(constraint, "c2")
+                
+        # eine Prüfung, die in mehreren Räumen abgehalten wird, findet nur zu einem Zeitpunkt statt
+        for i in range(n):
+            for k in range(r):
+                for l in range(p):
+                    constraint = (gb.quicksum([self.vars['x'][i, mu, m] for m in range(p) for mu in range(r) if (m != l and mu != k)]) <= r)
+                    self.problem.addConstr(constraint, "c3")
+             
+        # No conflicts
+        for i in range(n):
+            for k in range(r):
+                for l in range(p):
+                    constraint = (
+                        gb.quicksum([self.constants['Q'][i][j] * self.vars['x'][j, k, l] for j in range(n) if j != i]) <=
+                        (1 - self.vars['x'][i, k, l]) * sum(self.constants['Q'][i])
+                    )
+                    self.problem.addConstr(constraint, "c4")
+        
