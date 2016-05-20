6c6a18c91508cc9078f3d175a379cc8b35da64e6
mickael <mickael@cliqz.com>
2016-05-20 09:16:39 +0200

    print function added

---
+++ model/cuting_plane_problem.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This script contains a cuting plane approach: We take the LinearProblem and supress the third constraint
+# At each step we add a part from this third constraint.
+# If the third constraint isn't violated we have the optimal solution
+
+from model.main_problem import MainProblem
+import gurobipy as gb
+from utils.tools import convert_to_table
+
+
+class MasterProblem(MainProblem):
+    """ Master problem: without the third constraint
+    """
+    def __init__(self, data, name='MasterProblem'):
+        super(MasterProblem, self).__init__(name=name)
+        self.build_problem(data)
+        self.c = 0.5
+
+    def build_variables(self):
+        """ @param n, r, p: number of exams, rooms and periods
+            Build the variables of the problem from the data
+        """
+        n, r, p = self.dimensions['n'], self.dimensions['r'], self.dimensions['p']
+        self.vars.setdefault('x', {})
+        self.vars.setdefault('y', {})
+        self.vars.setdefault('z', {})
+        for i in range(n):
+            for k in range(r):
+                # exam i in room k
+                self.vars['x'][i, k] = self.problem.addVar(vtype=gb.GRB.BINARY, name='x[%s, %s]' % (i, k))
+            for l in range(p):
+                # exam i during period l
+                self.vars['y'][i, l] = self.problem.addVar(vtype=gb.GRB.BINARY, name='y[%s, %s]' % (i, l))
+        return True
+
+    def build_constraints(self):
+        """ @param n, r, p: number of exams, rooms and periods
+            Build the variables of the problem from the data
+        """
+        n, r, p = self.dimensions['n'], self.dimensions['r'], self.dimensions['p']
+        for i in range(n):
+            # Each exam i is scheduled on exactly one period
+            constraint = (gb.quicksum([self.vars['y'][i, l] for l in range(p)]) == 1)
+            self.problem.addConstr(constraint, "c0")
+            # enough seats for each student for exam i
+            constraint = (
+                gb.quicksum([self.vars['x'][i, k] * self.constants['c'][k] for k in range(r)]) >=
+                self.constants['s'][i]
+            )
+            self.problem.addConstr(constraint, "c1")
+            # No conflicts
+            for l in range(p):
+                constrant = (
+                    gb.quicksum([self.vars['y'][j, l] * self.constants['Q'][i][j] for j in range(n) if j != i]) <=
+                    (1 - self.vars['y'][i, l]) * n
+                )
+                self.problem.addConstr(constrant, "c2")
+            for k in range(r):
+                for l in range(p):
+                    if self.constants['T'][k][l] == 0:
+                        # We use room k if and only if the room is open
+                        constraint = (self.vars['x'][i, k] + self.vars['y'][i, l] <= 1)
+                        self.problem.addConstr(constraint, "c3")
+        return True
+
+    def build_objectif(self):
+        """ @param n, r, p: number of exams, rooms and periods
+            Build the constants of the problem from the data
+        """
+        n, r = self.dimensions['n'], self.dimensions['r']
+        crit1 = (
+            self.c * gb.quicksum([self.vars['x'][i, k] * self.constants['s'][i] for i in range(n) for k in range(r)])
+        )
+        crit2 = (
+            gb.quicksum([self.constants['Q'][i, j] * gb.quicksum([self.constants['h'][l] * (self.vars['y'][i, l] - self.vars['y'][j, l]) * (self.vars['y'][i, l] - self.vars['y'][j, l]) for l in range(p)])
+                         for i in range(n) for j in range(i + 1, n)])
+        )
+        self.problem.setObjective(crit1 - crit2 * crit2, gb.GRB.MINIMIZE)
+        return True
+
+    def __str__(self):
+        # Dimensions
+        res = 'DIMENSIONS: %s\n' % ', '.join('%s=%s' % (name, value)
+                                             for name, value in self.dimensions.iteritems())
+        # Variables
+        res += 'VARIABLES: \n'
+        # ----- x -------
+        res += '-------- x --------  lines=rooms, columns=exams\n'
+        res += convert_to_table(self.vars['x'], self.dimensions['n'], self.dimensions['r'])
+        # ----- y -------
+        res += '-------- y --------  lines=periods, columns=exams\n'
+        res += convert_to_table(self.vars['y'], self.dimensions['n'], self.dimensions['p'])
+        # Constants
+        res += '\nConstants: '
+        first = True
+        for name, consts in self.constants.iteritems():
+            if not first:
+                res += '\n           '
+            res += '%s=%s' % (name, str(consts))
+            first = False
+        return res
