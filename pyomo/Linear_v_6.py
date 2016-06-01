584ec2a7ed50b4c2f780f7c8e4d91f2931cff235
wedgeCountry <halbig.roland@freenet.de>
2016-06-01 18:13:58 +0200

    created Docu.tex, merge master

--- pyomo/Linear_v_6.py
+++ pyomo/Linear_v_6.py
@@ -24,9 +24,9 @@ from model.instance import build_random_data
 
 '''
 
-        -Model GurobiLinear_v_3 has fewer variables since it doesnt create x_(i,k,l) if room k is closed in period l
-        -Model GurobiLinear_v_4_Cliques adds Clique constraints for conflicts only one exam in a clique conflict can take place at a time
-        -Model GurobiLinear_v_4_Cliques changed r in BIG-M-Method  to 12 so far
+        -Model Linear_v_3 has fewer variables since it doesnt create x_(i,k,l) if room k is closed in period l
+        -Model Linear_v_4_Cliques adds Clique constraints for conflicts only one exam in a clique conflict can take place at a time
+        -Model Linear_v_4_Cliques changed r in BIG-M-Method  to 12 so far
 
 '''
 
@@ -60,55 +60,53 @@ from model.instance import build_random_data
 def build_model(data, n_cliques = 0):
     
     # Load Data Format
-    n = data['n']
-    r = data['r']
-    p = data['p']
-    s = data['s']
-    c = data['c']
-    h = data['h']
     conflicts = data['conflicts']
     locking_times = data['locking_times']
     T = data['T']
-    # sR = find_smallest_room_to_fit_in(n=n,s=s,r=r,c=c)
-    #print sR
     
+    # create abstract model
     model = pymo.AbstractModel()
     
-    print("Building variables...")
-    model.n = pymo.Param(initialize = n)
-    model.r = pymo.Param(initialize = r)
-    model.p = pymo.Param(initialize = p)
+    # setting dimensions
+    model.n = pymo.Param(initialize = data['n'])
+    model.r = pymo.Param(initialize = data['r'])
+    model.p = pymo.Param(initialize = data['p'])
     
+    # setting ranges
     model.N = pymo.RangeSet(1, model.n)
     model.R = pymo.RangeSet(1, model.r)
     model.P = pymo.RangeSet(1, model.p)
-    
+
+    # setting range sets
     model.NxN = model.N * model.N
     model.NxP = model.N * model.P
     model.RxP = model.R * model.P
     model.NxRxP = model.N * model.RxP
     
-    model.s = pymo.Param(model.N, initialize=lambda m, i: s[i-1])
-    model.c = pymo.Param(model.R, initialize=lambda m, k: c[k-1])
-    model.h = pymo.Param(model.P, initialize=lambda m, l: h[l-1])
-    model.H = pymo.Param(initialize=h[len(h)-1])
+    # setting constants
+    model.s = pymo.Param(model.N, initialize=lambda m, i: data['s'][i-1])
+    model.c = pymo.Param(model.R, initialize=lambda m, k: data['c'][k-1])
+    model.h = pymo.Param(model.P, initialize=lambda m, l: data['h'][l-1])
+    model.H = pymo.Param(initialize=data['h'][len(data['h'])-1])
     
-    model.conflicts = pymo.Set(model.NxN, filter = lambda i, j: j in conflicts)
     model.cliques = pymo.RangeSet(1, n_cliques)
     model.cxP = model.cliques * model.P
      
+    # setting variables
     model.x = pymo.Var(model.NxRxP, domain=pymo.Binary)
     model.y = pymo.Var(model.NxP, domain=pymo.Binary)
     
+    print("Building constraints...")
+    '''
+    The constraints are built in the pyomo setup. 
+    Constraints with appended _v are value based constraints and can be used for testing purposes
+    '''
+    
     print("c1: connecting variables x and y")
-    def c1a(m, i, l):
-        return 
-    def c1b(m, i, l):
-        return 
-    model.cons_1a = pymo.Constraint(model.NxP, rule=lambda m, i, l: sum(m.x[i, k, l] for k in m.R) <= m.r * m.y[i,l])
-    model.cons_1b = pymo.Constraint(model.NxP, rule=lambda m, i, l: sum(m.x[i, k, l] for k in m.R) >= m.y[i,l])
-    model.cons_1a_v = lambda m, i, l: sum(m.x[i, k, l].value for k in m.R) <= m.r * m.y[i,l].value
-    model.cons_1b_v = lambda m, i, l: sum(m.x[i, k, l].value for k in m.R) >= m.y[i,l].value
+    model.cons_1a = pymo.Constraint(model.NxP, rule=lambda m, i, l: sum(m.x[i, k, l] for k in m.R if T[k-1][l-1] == 1) <= m.r * m.y[i,l])
+    model.cons_1b = pymo.Constraint(model.NxP, rule=lambda m, i, l: sum(m.x[i, k, l] for k in m.R if T[k-1][l-1] == 1) >= m.y[i,l])
+    model.cons_1a_v = lambda m, i, l: sum(m.x[i, k, l].value for k in m.R if T[k-1][l-1] == 1) <= m.r * m.y[i,l].value
+    model.cons_1b_v = lambda m, i, l: sum(m.x[i, k, l].value for k in m.R if T[k-1][l-1] == 1) >= m.y[i,l].value
     
     print("c2: each exam at exactly one time")
     model.cons_2 = pymo.Constraint(model.N, rule=lambda m, i: sum(m.y[i,l] for l in m.P) == 1)
@@ -116,18 +114,16 @@ def build_model(data, n_cliques = 0):
     
     print("c3: avoiding conflicts")
     def c3(m, i, l):
-        if sum(m.y[j,l] for j in conflicts[i]) <= (1 - m.y[i,l]) * sum(conflicts[i]):
+        if sum(m.y[j,l] for j in conflicts[i-1]) <= (1 - m.y[i,l]) * sum(conflicts[i-1]):
             return pymo.Constraint.Feasible
     model.cons_3 = pymo.Constraint(model.NxP, rule=c3)
-    model.cons_3_v = lambda m, i, l: sum(m.y[j,l].value for j in conflicts[i]) <= (1 - m.y[i,l].value) * sum(conflicts[i])
+    model.cons_3_v = lambda m, i, l: sum(m.y[j,l].value for j in conflicts[i-1]) <= (1 - m.y[i,l].value) * sum(conflicts[i-1])
     
     print("c4: seats for all students")
-    model.cons_4 = pymo.Constraint(model.N, rule=lambda m, i: sum(m.x[i,k,l] * m.c[k] for (k,l) in m.RxP) >= m.s[i])
-    model.cons_4_v = lambda m, i: sum(m.x[i,k,l].value * m.c[k] for (k,l) in m.RxP) >= m.s[i]
+    model.cons_4 = pymo.Constraint(model.N, rule=lambda m, i: sum(m.x[i,k,l] * m.c[k] for (k,l) in m.RxP if T[k-1][l-1] == 1) >= m.s[i])
+    model.cons_4_v = lambda m, i: sum(m.x[i,k,l].value * m.c[k] for (k,l) in m.RxP if T[k-1][l-1] == 1) >= m.s[i]
     
     print("c5: only one exam per room per period")
-    def c5(m, k, l):
-        return 
     model.cons_5 = pymo.Constraint(model.RxP, rule= lambda m, k, l: sum(m.x[i,k,l] for i in m.N) <= 1 )
     model.cons_5_v = lambda m, k, l: sum(m.x[i,k,l].value for i in m.N) <= 1 
     
@@ -140,10 +136,10 @@ def build_model(data, n_cliques = 0):
     print("c7: Building %d clique constraints" %n_cliques)
     if n_cliques > 0:
         G = nx.Graph()
-        for i in range(n):
+        for i in range(data['n']):
             G.add_node(i)  
-        for i in range(n):
-            for j in conflicts[i]:
+        for i in range(data['n']):
+            for j in conflicts[i-1]:
                 G.add_edge(i,j)
         
         cliques = [] # generator
@@ -153,25 +149,19 @@ def build_model(data, n_cliques = 0):
                 break
             cliques.append(clique)
             counter += 1
-        #for clique in cliques:
-            #print clique
-        #print("____")
-        #print(len(cliques))
         
-        def c3(m, i, l):
-            return sum([m.y[j+1,l] for j in cliques[i-1]]) <= 1
-                
-        model.cons_7 = pymo.Constraint(model.cxP, rule=c3)
+        model.cons_7 = pymo.Constraint(model.cxP, rule=lambda m, i, l: sum([m.y[j+1,l] for j in cliques[i-1]]) <= 1)
         
     
     print("Building Objective...")
-    model.OBJ = pymo.Objective(sense=pymo.minimize, rule = lambda m: sum(m.x[i,k,l] for i,k,l in m.NxRxP))
-    model.OBJ_v = lambda m: sum(m.x[i,k,l].value/m.s[i] for i,k,l in m.NxRxP)
+    model.OBJ = pymo.Objective(sense=pymo.minimize, rule = lambda m: sum(m.x[i,k,l] for i,k,l in m.NxRxP if T[k-1][l-1] == 1))
+    model.OBJ_v = lambda m: sum(m.x[i,k,l].value for i,k,l in m.NxRxP if T[k-1][l-1] == 1)
     
+    # initialize model
     print("Creating instance...")
     instance = model.create_instance()
     
-    print("OK - Model built")
+    print("OK - Model built!")
     return(instance)
 
 from pyomo.opt import SolverFactory
@@ -194,22 +184,33 @@ def schedule_exams(data, solver_name="gurobi", n_cliques=0, print_results=False)
     print optimizer.options
     
     t = time()
-    instance = build_model(data, n_cliques = 0)        
+    instance = build_model(data, n_cliques = n_cliques)        
     print("Solving...")
     results = optimizer.solve(instance)
     t = time() - t   
     
     instance.solutions.load_from(results)
-    x = [instance.x[i,k,l].value for (i,k,l) in instance.NxRxP]
-    y = [instance.y[i,l].value for (i,l) in instance.NxP]
-    objVal = instance.OBJ_v(instance)
     
-    if is_integer_solution(instance):
+    is_integral = is_integer_solution(instance)
+    if is_integral:
         print "All integer solution!"
-        x = map(int, x)
-        y = map(int, y)
+    
+    y = {}
+    for (i,l) in instance.NxP:
+        if is_integral:
+            y[i,l] = int(instance.y[i,l].value)
+        else:
+            y[i,l] = instance.y[i,l].value
         
-    objVal = sum(x)
+    x = {}
+    for (i,k,l) in instance.NxRxP:
+        if y[i,l] == 1:
+            if is_integral:
+                x[i,k] = int(instance.x[i,k,l].value)
+            else:
+                x[i,k] = instance.x[i,k,l].value
+        
+    objVal = instance.OBJ_v(instance)
     
     if print_results:
         print instance.display()
@@ -220,18 +221,20 @@ def schedule_exams(data, solver_name="gurobi", n_cliques=0, print_results=False)
     
 if __name__ == "__main__":
     
-    n = 170
+    n = 300
     r = 20
-    p = 10
+    p = 15
 
     # generate data
     random.seed(42)
-    data = build_random_data(n=n, r=r, p=p, prob_conflicts=0.5)
+    data = build_random_data(n=n, r=r, p=p, prob_conflicts=0.35)
     exams = [ 'Ana%s' % (i+1) for i in range(n) ]
     rooms = ['MI%s' % (k+1) for k in range(r)]
     
+    random.seed(42)
     model, x, y, objVal, t = schedule_exams(data, n_cliques=0, solver_name = "gurobi")
     
+    random.seed(42)
     model2, x2, y2, objVal2, t2 = schedule_exams(data, n_cliques=0, solver_name = "glpk")
     
     print('Runtime: %0.2f s' % t)
