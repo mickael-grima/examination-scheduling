e1bb470c61e554a4dbce03288921777230308aed
Max <adobetest1@gmx.de>
2016-06-05 11:56:07 +0200

    Coloring now fully considers seating capacities and number of rooms available

--- heuristics/graph_coloring.py
+++ heuristics/graph_coloring.py
@@ -16,16 +16,28 @@ import itertools
 def greedy_coloring(data, G, visiting_scheme):
     
     # TODO: Implement greedy graph coloring algorithm which uses room capacities as boundary conditions
+        #DONE
+        #Might be worth in combining coloring with ordering
+
     
     # TODO: Use ColoGraph API for this, please. Might come in handy!
+       #TO DISCUSS
     
     # TODO: Consider constraints for feasible rooms
+        #Currrent heuristic
+        #Maybe: Solving LP for chcking feasability
+
+
     
     # Dictionarry for final coloring
     coloring = {}
 
     # Keep track of the number of students that have to write in a given period/color for capacity checking
     total_students = {}
+    total_rooms = {}
+
+    capacity = sum(data['c'])
+    print capacity
 
     for node in visiting_scheme:
         # Make sure he colors of all the neigbour are known
@@ -36,10 +48,15 @@ def greedy_coloring(data, G, visiting_scheme):
             if neighbour in coloring:
                 neighbour_coloring.add(coloring[neighbour])
 
-        # Find first color that is not in neighbourhood and which would not exceed room capacity for a given period
+        # Find first color that is not in neighbourhood and which would not exceed room capacity for a given period (by seats and number of room)
         for color in itertools.count():
             if color not in neighbour_coloring:
-                break
+                if color in total_students:
+                    if total_students[color] + data['s'][node] <= capacity and total_rooms[color] + 1 <= data['r']:
+                        break
+                else:
+                    if data['s'][node] <= capacity:
+                        break
 
         # Set found color
         coloring[node] = color
@@ -47,8 +64,10 @@ def greedy_coloring(data, G, visiting_scheme):
         # Update number of students in given period
         if color in total_students:
             total_students[color] += data['s'][node]
+            total_rooms[color] += 1
         else:
             total_students[color] = data['s'][node]
+            total_rooms[color] = 1
     
     print data['s']    
     print total_students
@@ -67,7 +86,7 @@ if __name__ == '__main__':
 
     G = nx.Graph()
     G.add_nodes_from([0,1,2,3,4,5,6,7,8,9])
-    G.add_edges_from([(1,2),(1,3),(2,4),(2,3),(3,5),(5,6),(1,6),(3,6)])
+    #G.add_edges_from([(1,2),(1,3),(2,4),(2,3),(3,5),(5,6),(1,6),(3,6)])
 
     visiting_scheme=[1,2,3,4,5,6,0,7,8,9]
 
