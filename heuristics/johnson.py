606d0af91a5df04e7fd2d43b58a9685eabc7c71f
axeckl <axeckl@gmail.com>
2016-06-14 16:27:00 +0200

    Implemented first steps of Johnson.py

--- heuristics/johnson.py
+++ heuristics/johnson.py
@@ -11,8 +11,11 @@ sys.path.append(PROJECT_PATH)
 import networkx as nx
 import numpy as np
 
-from heuristics.AC import AC
-from heuristics.graph_coloring import greedy_coloring
+from operator import itemgetter
+
+from ConstrainedColorGraph import ConstrainedColorGraph
+# from heuristics.AC import AC
+# from heuristics.graph_coloring import greedy_coloring
 
 
 #
@@ -21,14 +24,39 @@ from heuristics.graph_coloring import greedy_coloring
 
 class Johnson:
     
-    def __init__(self, data):
+    def __init__(self, data,n_colorings=50):
         self.data = data
+        self.n_colorings = n_colorings
+        self.graph = ConstrainedColorGraph()
+        self.graph.build_graph(self.data['n'], self.data['conflicts'])
+
     def generate_colorings(self):
-        # TODO: Generate colourings
-        conflicts = self.data['conflicts']
-        return [ get_coloring(conflicts) ]
+        # Generate colourings using Johnson's rule: 
+        # Order the exams by alpha*s_i + conf_numb
+
+        colorings = []
+
+        for i in range(self.n_colorings):
+            # reset node ordering and coloring
+            nodes = self.graph.nodes()
+            self.graph.reset_colours()
+
+            # set parameter alpha = {0.01,0.02, ... ,0.5} and compute exam value for ordering
+            alpha = (i+1)*0.01
+            vals = [alpha*d for d in data['s']]
+
+            # sort nodes by vals
+            nodes = [elmts[0] for elmts in sorted(zip(nodes, vals), key=itemgetter(1), reverse=True)]
+
+            # compute coloring
+            for node in nodes:
+                self.graph.color_node(node, data=self.data, check_constraints = False)
+            colorings.append({n: c for n, c in self.graph.colours.iteritems()}) 
+
+        return colorings
+
     def update(self, values, best_index = None):
-        #print "Do something. Value is", values[best_index]
+        # no update necessary yet as of now
         pass
         
 
@@ -43,7 +71,7 @@ if __name__ == '__main__':
     data = build_smart_random(n=n, r=r, p=p, tseed=tseed) 
 
     num_ants = 10
-    ac = Johnson(data)
-    colorings = ac.generate_colorings(num_ants)
-    print colorings
+    js = Johnson(data)
+    colorings = js.generate_colorings()
+    # print colorings
     
