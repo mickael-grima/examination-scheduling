4cfe836090166a75a208626001e8d309c3ede195
Max <adobetest1@gmx.de>
2016-06-18 21:06:45 +0200

    Adapted macro to add number of students participating to extra tab

--- input/readTumOnline.py
+++ input/readTumOnline.py
@@ -30,43 +30,6 @@ def read_real_data():
     Q = [[int(cell.value) if not cell.value is None else 0 for cell in row]   for row in ws.iter_rows("B2:%s%s" % (openpyxl.utils.get_column_letter(maxRow),maxRow))]
 
 
-
-	
-    # np.random.seed(kwards.get('tseed', 1))
-    # rd.seed(kwards.get('tseed', 1))
-    # n, r, p, w = kwards.get('n', 0), kwards.get('r', 0), kwards.get('p', 0), kwards.get('w', ["1", "2", "3", "4", "5", "6", "7"])
-    # data = {'n': n, 'r': r, 'p': p}
-
-    # #create possible number of participants, increase probability that number of participants is between 150 and 300
-    # num = [i for i in range(10,901)]
-    # for times in range(1500):
-    #     num.extend([int(i) for i in range(10,150)])
-    # for times in range(500):
-    #     num.extend([int(i) for i in range(150,301)])
-
-    # # get number of students participating
-    # data['s'] = np.random.choice(num, n)
-
-    # # get room capacity from real data
-    # data['c'] = np.random.choice(num, r)
-    # data['c'] = sorted(data['c'], reverse=True)
-
-    # if kwards.get('locations') == True:
-    # 	data['w'] = np.random.choice([["1"], ["2"], ["3"], ["2","3"], ["1","2"], ["1","3"], ["1","2","3"]], n , p=[0.2, 0.1, 0.05, 0.05, 0, 0, 0.6])
-    # 	data['location'] = np.random.choice(["1", "2", "3"], r , p=[0.6, 0.35, 0.05])
-    
-    # # hours between starting day and starting periods are fixed equal to 2
-    # data['h'] = [ 2*l for l in range(p)]
-    
-    # # create a conflict by probybility 1/5
-    # data['conflicts'] = defaultdict(list)
-    # for i in range(n):
-    #     data['conflicts'][i] = [ j for j in range(i+1,n) if rd.random() <= 0.1 ]
-    
-    # #close some rooms by probability 1/10
-    # data['locking_times'] = defaultdict(list)
-    # for k in range(r):
-    #     data['locking_times'][k] = [ l for l in range(p) if rd.random() <= 0.1 ]
     
     return 
 
