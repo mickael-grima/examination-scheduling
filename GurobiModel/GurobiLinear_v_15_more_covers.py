d4eb2ef6f478f00f19e4913e3c2061fd32750046
Max <adobetest1@gmx.de>
2016-06-19 15:22:31 +0200

    worker now save results to file

--- GurobiModel/GurobiLinear_v_15_more_covers.py
+++ GurobiModel/GurobiLinear_v_15_more_covers.py
@@ -165,16 +165,16 @@ def build_model(data, n_cliques = 0, verbose = True):
 
     model.params.OutputFlag = 1
     model.params.Method = 3
-    model.params.MIPFocus = 1
+    #model.params.MIPFocus = 1
 
 
     # cuts
-    model.params.cuts = 0
+    #model.params.cuts = 0
     #model.params.coverCuts = 2
     #model.params.CutPasses = 4
 
     # heuristics
-    model.params.heuristics = 0
+    #model.params.heuristics = 0
 
     #model.params.symmetry = 2
 
