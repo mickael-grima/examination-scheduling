2fe77db6ce9f200cd8d24f90713fd2c49c318eb6
wedgeCountry <halbig.roland@freenet.de>
2016-06-26 13:56:53 +0200

    Removed asssert in color_node

--- evaluation/moses.py
+++ evaluation/moses.py
@@ -35,60 +35,61 @@ def obj_time(times, data, h_max = None):
         return distance_sum
     
 
-
-
 if __name__ == '__main__':
     
     gamma = 1.0
     
     data = examination_data.read_data(threshold = 0)
-    
     data['similar_periods'] = tools.get_similar_periods(data)
+    h = data['h']
     
     n, r, p = data['n'], data['r'], data['p']
     print n, r, p
     
+    # for each exam the time
     exam_times = data['exam_times']
+    # for each exam the room
     exam_rooms = data['exam_rooms']
+    # for each room index the name
     room_names = data['room_names']
-    h = data['h']
-    x = defaultdict(int)
+    
     y = defaultdict(int)
     for i, exam in enumerate(exam_times):
         l = h.index(exam_times[exam])
         y[i,l] = 1.0
     
-    #for room in exam_rooms.values():
-        #assert room in room_names.values()
-    
-    
-    rooms_in_moses = set()
-    
-    illicit_rooms = set()
-    penalty = 0
-    
-    print "Rooms of which we have data:"
-    print sorted(room_names.values())
+    x = defaultdict(int)
+    Tcount = 0
+    exam_count = 0
     for i, exam in enumerate(exam_rooms):
-        for room in exam_rooms[exam]:
-            rooms_in_moses.add(room)
-            if room in room_names.values():
+        # if exam is plannable with our data, set variable
+        if all(room in room_names.values() for room in exam_rooms[exam]):
+            for room in exam_rooms[exam]:
                 k = room_names.values().index(room)
                 x[i,l] = 1.0
-            else:
-                illicit_rooms.add(room)
-                penalty += 1
-    print "Rooms in moses:"
-    print sorted(rooms_in_moses)
-    
-    print "Rooms of which we don't have data:"
-    print sorted(illicit_rooms)
-    
-    r1 = set([ room for rooms in exam_rooms.values() for room in rooms ])
-    r2 = set(room_names.values())
-    print sorted([r for r in r1 if r not in r2])
-    
-    v = obj1(x) + penalty - gamma * obj_time(exam_times.values(), data, h_max = max(h))
-    
+        # otherwise lock the corresponding room
+        else:
+            exam_count += 1
+            for room in exam_rooms[exam]:
+                Tcount += 1
+                if room in room_names.values():
+                    k = room_names.values().index(room)
+                    l = h.index(exam_times[exam])
+                    data['T'][k][l] = 0.0
+          
+    print Tcount, exam_count
+    v = obj1(x) - gamma * obj_time(exam_times.values(), data, h_max = max(h))
     print "VALUE:", v
     
+    if False:
+        
+        print "Rooms of which we have data:"
+        print sorted(room_names.values())
+        print "Rooms in moses:"
+        r1 = set([ room for rooms in exam_rooms.values() for room in rooms ])
+        print sorted(r1)
+        print "Rooms of which we don't have data:"
+        r2 = set(room_names.values())
+        print sorted([r for r in r1 if r not in r2])
+        
+        
